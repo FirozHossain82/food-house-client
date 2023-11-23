838e6528c77f39766b01c1f7026208e271710e98
FirozHossain82 <firozhossain295@gmail.com>
2023-11-23 10:45:15 +0600

    project setup and footer added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer.jsx";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
